aa5b461bd953e3df85cf6028822b17726911efed
Vitaly Rajev <vitaly.rajev@gmail.com>
2020-07-10 21:41:12 +0300

    created basic human player

--- DurakPlayer.py
+++ DurakPlayer.py
@@ -142,14 +142,42 @@ class DurakPlayerWithMemory(DurakPlayer):
 
 class HumanPlayer(DurakPlayer):
 
+    def __init__(self, hand_size: int, name: str, gui):
+        DurakPlayer.__init__(self, hand_size, name)
+        self.__game_gui = gui
+
     def attack(self, table: Tuple[List[Deck.CardType], List[Deck.CardType]], legal_cards_to_play: List[Deck.CardType]) -> Deck.CardType:
-        attacking_card = self.__get_lowest_card(legal_cards_to_play)
+        waiting = True
+        attacking_card = Deck.NO_CARD
+        while waiting:
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONUP:
+                    if event.button == 1:
+                        pressed_card = self.__get_pressed_card()
+                        if pressed_card != Deck.NO_CARD:
+                            if pressed_card in legal_cards_to_play:
+                                attacking_card = pressed_card
+                                waiting = False
+                    elif event.button == 3:
+                        waiting = False
         if attacking_card != Deck.NO_CARD:
             self._hand.remove(attacking_card)
         return attacking_card
 
     def defend(self, table: Tuple[List[Deck.CardType], List[Deck.CardType]], legal_cards_to_play: List[Deck.CardType]) -> Deck.CardType:
-        defending_card = self.__get_lowest_card(legal_cards_to_play)
+        waiting = True
+        defending_card = Deck.NO_CARD
+        while waiting:
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONUP:
+                    if event.button == 1:
+                        pressed_card = self.__get_pressed_card()
+                        if pressed_card != Deck.NO_CARD:
+                            if pressed_card in legal_cards_to_play:
+                                defending_card = pressed_card
+                                waiting = False
+                    elif event.button == 3:
+                        waiting = False
         if defending_card != Deck.NO_CARD:
             self._hand.remove(defending_card)
         return defending_card
@@ -166,3 +194,18 @@ class HumanPlayer(DurakPlayer):
             elif lowest_card[1] == self._trump_rank:
                 lowest_card = card
         return lowest_card
+
+    def __get_pressed_card(self):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        card_w, card_h = self.__game_gui.card_size
+        positions = self.__game_gui.human_player_cards_positions
+        pressed_card = Deck.NO_CARD
+        if (positions[0][0] <= mouse_x < (positions[-1][0] + card_w)) and (positions[0][1] <= mouse_y <= (positions[0][1] + card_h)):
+            for i, (x, _) in enumerate(positions):
+                if i < (len(positions) - 1):
+                    if x <= mouse_x < min(x + card_w, positions[i + 1][0]):
+                        pressed_card = self._hand[i]
+                else:
+                    if x <= mouse_x <= (x + card_w):
+                        pressed_card = self._hand[i]
+        return pressed_card
