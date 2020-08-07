0e62057e6fadce6815230ec8454fffbf83cd6f76
Vitaly Rajev <vitaly.rajev@gmail.com>
2020-08-07 14:26:36 +0300

    fixed some documentation

--- DefensivePlayer.py
+++ DefensivePlayer.py
@@ -3,12 +3,17 @@ from DurakPlayer import DurakPlayer, Deck, Tuple, List, Optional
 
 class DefensivePlayer(DurakPlayer):
     """
-    The defensive player attacks with the *lowest* non trump card possible, or lowest trump card otherwise.
-    The defensive player defends with the lowest non trump card possible, or lowest trump card otherwise.
+    The defensive player always plays the weakest card available, preferring to play any card over not playing at all.
     """
 
     def attack(self, table: Tuple[List[Deck.CardType], List[Deck.CardType]], legal_cards_to_play: List[Deck.CardType]) -> Optional[Deck.CardType]:
-        return self.get_weakest_card(legal_cards_to_play)
+        chosen_card = self.get_weakest_card(legal_cards_to_play)
+        if chosen_card != Deck.NO_CARD:
+            self._hand.remove(chosen_card)
+        return chosen_card
 
     def defend(self, table: Tuple[List[Deck.CardType], List[Deck.CardType]], legal_cards_to_play: List[Deck.CardType]) -> Optional[Deck.CardType]:
-        return self.get_weakest_card(legal_cards_to_play)
+        chosen_card = self.get_weakest_card(legal_cards_to_play)
+        if chosen_card != Deck.NO_CARD:
+            self._hand.remove(chosen_card)
+        return chosen_card
