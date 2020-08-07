14ec0e2220695ccf5325f5dbbe8a92ab8d8407b5
Vitaly Rajev <vitaly.rajev@gmail.com>
2020-08-07 12:14:41 +0300

    before removal

--- LearningPlayer.py
+++ LearningPlayer.py
@@ -1,30 +1,15 @@
 from DurakPlayer import DurakPlayer, Deck, Tuple, List, Optional
-from typing import Union
-import random
 
 
 class LearningPlayer(DurakPlayer):
-
-    def learn(self, prev_table: Tuple[List[Deck.CardType], List[Deck.CardType]], prev_action: Deck.CardType,
-              reward: float, next_table: Tuple[List[Deck.CardType], List[Deck.CardType]]) -> None:
-        raise NotImplementedError()
-
-    def batch_learn(self, batch: List[Tuple[Tuple[List[Deck.CardType], List[Deck.CardType]], Deck.CardType, Union[int, float], Tuple[List[Deck.CardType], List[Deck.CardType]]]]):
-        raise NotImplementedError()
 
     def learn_step(self, old_state, new_state, action, reward, info):
         raise NotImplementedError()
 
     def attack(self, table: Tuple[List[Deck.CardType], List[Deck.CardType]],
                legal_cards_to_play: List[Deck.CardType]) -> Optional[Deck.CardType]:
-        attacking_card = random.choice(legal_cards_to_play)
-        if attacking_card != Deck.NO_CARD:
-            self._hand.remove(attacking_card)
-        return attacking_card
+        raise NotImplementedError()
 
     def defend(self, table: Tuple[List[Deck.CardType], List[Deck.CardType]],
                legal_cards_to_play: List[Deck.CardType]) -> Optional[Deck.CardType]:
-        defending_card = random.choice(legal_cards_to_play)
-        if defending_card != Deck.NO_CARD:
-            self._hand.remove(defending_card)
-        return defending_card
+        raise NotImplementedError()
