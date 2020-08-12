418b14d29f1cdfa738a31ebfba9ca09b6ea2b0db
Vitaly Rajev <vitaly.rajev@gmail.com>
2020-08-12 12:15:32 +0300

    Random Player done

--- RandomPlayer.py
+++ RandomPlayer.py
@@ -1,25 +1,23 @@
-from DurakPlayer import DurakPlayer, Deck, Tuple, List, Optional
+from Types import CardType, CardListType, TableType
+from DurakPlayer import DurakPlayer
+from Deck import Deck
+
 import random
 
 
 class RandomPlayer(DurakPlayer):
     """
-    Random agent
+    Random agent.
+    Chooses a random action from the given legal actions to play.
     """
 
-    def attack(self, table: Tuple[List[Deck.CardType], List[Deck.CardType], int, List[int]], legal_cards_to_play: List[Deck.CardType]) -> Optional[Deck.CardType]:
-        """
-        chooses a random card from legal cards to play
-        """
+    def attack(self, table: TableType, legal_cards_to_play: CardListType) -> CardType:
         attacking_card = random.choice(legal_cards_to_play)
         if attacking_card != Deck.NO_CARD:
             self._hand.remove(attacking_card)
         return attacking_card
 
-    def defend(self, table: Tuple[List[Deck.CardType], List[Deck.CardType], int, List[int]], legal_cards_to_play: List[Deck.CardType]) -> Optional[Deck.CardType]:
-        """
-        chooses a random card from legal cards to play
-        """
+    def defend(self, table: TableType, legal_cards_to_play: CardListType) -> CardType:
         defending_card = random.choice(legal_cards_to_play)
         if defending_card != Deck.NO_CARD:
             self._hand.remove(defending_card)
