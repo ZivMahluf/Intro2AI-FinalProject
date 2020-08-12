47885967bafa109cb63cc123d24df9c492df73a8
Vitaly Rajev <vitaly.rajev@gmail.com>
2020-08-12 12:13:13 +0300

    NFSP Super Player needs documentation

--- NFSPSuperPlayer.py
+++ NFSPSuperPlayer.py
@@ -1,21 +1,22 @@
+from Types import NumberType, CardType, CardListType, TableType, StateType
 from NFSPPlayer import NFSPPlayer
 
 
 class NFSPSuperPlayer(NFSPPlayer):
-    def __init__(self, hand_size, name, device='cpu'):
-        super().__init__(hand_size,name,device)
+    def __init__(self, hand_size: int, name: str, device: str = 'cpu'):
+        super().__init__(hand_size, name, device)
         self.attackPlayer = NFSPPlayer(hand_size, name, device)
         self.defendPlayer = NFSPPlayer(hand_size, name, device)
         self.attacked = False
 
-    def attack(self, table, legal_cards_to_play):
+    def attack(self, table: TableType, legal_cards_to_play: CardListType) -> CardType:
         self.attacked = True
         self.attackPlayer._hand = self._hand
         act = self.attackPlayer.attack(table, legal_cards_to_play)
         self._hand = self.attackPlayer._hand
         return act
 
-    def defend(self, table, legal_cards_to_play):
+    def defend(self, table: TableType, legal_cards_to_play: CardListType) -> CardType:
         self.attacked = False
         self.defendPlayer._hand = self._hand
         act =  self.defendPlayer.defend(table, legal_cards_to_play)
@@ -32,10 +33,8 @@ class NFSPSuperPlayer(NFSPPlayer):
         self.attackPlayer.save_network(name+"attack")
         self.defendPlayer.save_network(name+"defend")
 
-    def learn_step(self, old_state, new_state, action, reward):
+    def learn_step(self, old_state: StateType, new_state: StateType, action: CardType, reward: NumberType) -> None:
         if self.attacked:
-            self.attackPlayer.learn_step(
-                old_state, new_state, action, reward)
+            self.attackPlayer.learn_step(old_state, new_state, action, reward)
         else:
-            self.defendPlayer.learn_step(
-                old_state, new_state, action, reward)
+            self.defendPlayer.learn_step(old_state, new_state, action, reward)
