da6482cda312063c1cab874f2afb7cc7e7a0adb5
idan-yamin <idan.yamin781@gmail.com>
2020-08-14 21:41:59 +0300

    fixed graph

--- graph_script.py
+++ graph_script.py
@@ -26,16 +26,16 @@ def plot(x_axis, y_axes, title: str, x_label: str, y_label: str, legend: bool):
         plt.legend()
 
 
-def graph(dir, title, epochs=100, training_games_per_epoch=50, test_games_per_epoch_vs_test_players=500):
+def graph(dir, title, file_name, epochs=100, training_games_per_epoch=50, test_games_per_epoch_vs_test_players=500):
     cumulative_training_games_per_epoch = list(range(
-        training_games_per_epoch, training_games_per_epoch * epochs + 1, training_games_per_epoch))
+        training_games_per_epoch, training_games_per_epoch * epochs, training_games_per_epoch))
     loss_ratio_vs_1_random = []
     loss_ratio_vs_3_random = []
     trained = TrainedNFSPPlayer(hand_size, 'trained')
     random1 = RandomPlayer(hand_size, 'random-1')
     random2 = RandomPlayer(hand_size, 'random-2')
     random3 = RandomPlayer(hand_size, 'random-3')
-    for epoch in range(epochs):
+    for epoch in range(epochs-1):
         trained.load_model(dir+'epoc-' + str(epoch + 1))
         loss_ratios = do_test_games(
             [trained, random1], test_games_per_epoch_vs_test_players)
@@ -49,10 +49,11 @@ def graph(dir, title, epochs=100, training_games_per_epoch=50, test_games_per_ep
         colors[2], loss_ratio_vs_3_random)}
     plot(cumulative_training_games_per_epoch, learning_player_info,
          title, "Number of training games", "Loss Ratio", True)
+    plt.savefig(file_name)
     plt.show()
 
 
 if __name__ == '__main__':
-    graph("train_against_nfsp/", "NFSP trained against another NFSP")
-    graph("train_against_prev_iter/", "NFSP trained against previous iterations")
-    graph("train_against_one_random/", "NFSP trained against random player")
+    graph("train_against_nfsp/", "NFSP trained against another NFSP", "1.jpg")
+    graph("train_against_prev_iter/", "NFSP trained against previous iterations", "2.jpg")
+    graph("train_against_one_random/", "NFSP trained against random player", "3.jpg")
