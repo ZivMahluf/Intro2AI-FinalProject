9557948661f1c9cef336fea40a48a438054aafe7
Vitaly Rajev <vitaly.rajev@gmail.com>
2020-08-15 14:41:16 +0300

    added ppo training-testing-plots

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from RandomPlayer import RandomPlayer
 from HumanPlayer import HumanPlayer
 from DurakPlayer import DurakPlayer
 from NFSPPlayer import NFSPPlayer
-from PPOPlayer import PPOPlayer
+from PPOPlayer import PPOPlayer, from_path
 
 from Types import List
 from NFSPTrainer import NFSPTrainer
@@ -16,6 +16,7 @@ from DurakEnv import DurakEnv
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
+import logging
 import random
 import shutil
 import joblib
@@ -32,7 +33,7 @@ nfsp_saved_models_dir = os.path.join(os.getcwd(), "NFSP-models")
 ppo_saved_models_dir = os.path.join(os.getcwd(), "PPOParams")
 
 
-def do_test_games(players: List[DurakPlayer], num_games: int):
+def do_test_games(players: List[DurakPlayer], num_games: int, steps_lim=500):
     loss_ratios = {player.name: 0 for player in players}
     env = DurakEnv(players, False)
     print_every = min(num_games // 10, 50)
@@ -42,9 +43,11 @@ def do_test_games(players: List[DurakPlayer], num_games: int):
                 print("playing test game", i, "out of", num_games)
         state = env.reset()
         done = False
-        while not done:
+        step = 1
+        while not done and (step < steps_lim):
             act = (env.get_turn_player()).get_action(state, env.to_attack())
             state, _, done = env.step(act)
+            step += 1
         loser = env.get_loser()
         if loser is not None:
             loss_ratios[loser.name] += 1
@@ -55,7 +58,60 @@ def do_test_games(players: List[DurakPlayer], num_games: int):
     return loss_ratios
 
 
-def train_ppo(sess, games_per_batch, save_every, training_games):
+def get_sorted_filenames_and_indices():
+    files = [(int(f[5:]), f) for f in os.listdir(ppo_saved_models_dir) if f.find('model') != -1 and int(f[5:]) % 5000 == 0]
+    files.sort(key=lambda x: x[0])
+    return list(zip(*files))
+
+
+def run_against_one_random(params_filename, games):
+    with tf.compat.v1.Session() as sess:
+        player1 = from_path(sess, DurakEnv.HAND_SIZE, "Trained1_PPO_" + params_filename, os.path.join(ppo_saved_models_dir,  params_filename))
+        player2 = RandomPlayer(DurakEnv.HAND_SIZE, "RandomPlayer")
+        loss_ratios = do_test_games([player1, player2], games)
+        return loss_ratios[player1.name] / 100
+
+
+def run_against_3_randoms(params_filename, games):
+    with tf.compat.v1.Session() as sess:
+        player1 = from_path(sess, DurakEnv.HAND_SIZE, "Trained2_PPO_" + params_filename, os.path.join(ppo_saved_models_dir,  params_filename))
+        player2 = RandomPlayer(DurakEnv.HAND_SIZE, "RandomPlayer1")
+        player3 = RandomPlayer(DurakEnv.HAND_SIZE, "RandomPlayer2")
+        player4 = RandomPlayer(DurakEnv.HAND_SIZE, "RandomPlayer3")
+        loss_ratios = do_test_games([player1, player2, player3, player4], games)
+        return loss_ratios[player1.name] / 100
+
+
+def run_against_3_defensives(params_filename, games):
+    with tf.compat.v1.Session() as sess:
+        player1 = from_path(sess, DurakEnv.HAND_SIZE, "Trained3_PPO_" + params_filename, os.path.join(ppo_saved_models_dir,  params_filename))
+        player2 = DefensivePlayer(DurakEnv.HAND_SIZE, "DefensivePlayer1")
+        player3 = DefensivePlayer(DurakEnv.HAND_SIZE, "DefensivePlayer2")
+        player4 = DefensivePlayer(DurakEnv.HAND_SIZE, "DefensivePlayer3")
+        loss_ratios = do_test_games([player1, player2, player3, player4], games)
+        return loss_ratios[player1.name] / 100
+
+
+def run_against_3_aggressives(params_filename, games):
+    with tf.compat.v1.Session() as sess:
+        player1 = from_path(sess, DurakEnv.HAND_SIZE, "Trained4_PPO_" + params_filename, os.path.join(ppo_saved_models_dir,  params_filename))
+        player2 = AggressivePlayer(DurakEnv.HAND_SIZE, "AggressivePlayer1")
+        player3 = AggressivePlayer(DurakEnv.HAND_SIZE, "AggressivePlayer2")
+        player4 = AggressivePlayer(DurakEnv.HAND_SIZE, "AggressivePlayer3")
+        loss_ratios = do_test_games([player1, player2, player3, player4], games)
+        return loss_ratios[player1.name] / 100
+
+
+def plot_graph(title, indices, losses):
+    plt.figure()
+    plt.title(title)
+    plt.xlabel("Player Model Number")
+    plt.ylabel("Loss Ratios")
+    plt.ylim(0, 1)
+    plt.plot(indices, losses)
+
+
+def train_ppo():
     """
     Trains a PPO player.
     :param sess: tensorflow session
@@ -63,79 +119,55 @@ def train_ppo(sess, games_per_batch, save_every, training_games):
     :param save_every: frequency of saving the model parameters (saved every save_every batches)
     :param training_games: total number of training games to train for
     """
-    # Resets the folder of the saved parameters (uncomment to enable reset directory)
-    # if os.path.exists(ppo_saved_models_dir):
-    #     shutil.rmtree(ppo_saved_models_dir)
-    # os.mkdir(ppo_saved_models_dir)
-    trainer = PPOTrainer(sess,
-                         games_per_batch=games_per_batch,
-                         training_steps_per_game=25,
-                         learning_rate=0.00025,
-                         clip_range=0.2,
-                         save_every=save_every)
-    name = os.path.join(ppo_saved_models_dir, "model0")
-    trainer.trainingNetwork.saveParams(name)
-    trainer.train(training_games)
+    # Resets the folder of the saved parameters
+    if not os.path.exists(ppo_saved_models_dir):
+        os.mkdir(ppo_saved_models_dir)
+    logging.basicConfig(filename='logs/PPOTrainer_log', level=logging.INFO)
+    with tf.compat.v1.Session() as sess:
+        trainer = PPOTrainer(sess,
+                             games_per_batch=5,
+                             training_steps_per_game=25,
+                             learning_rate=0.00025,
+                             clip_range=0.2,
+                             save_every=100)
+        trainer.train(500000)
+    logging.shutdown()
 
 
-def plot_trained_ppo_player_vs_test_cases(sess, title, test_games_per_model=20):
-    """
-    Trains a PPO player for a given number of games.
-    After training, loads the saved models and tests them for the given number of games against the given test players.
-    The % of losses of each trained PPO player are plotted against the number of training games up to the point of saving the tested model (meaning we
-    plot the loss % of the player against the test players as a function of the training games).
-    :param sess: tensorflow session
-    :param title: title for the plot
-    :param test_games_per_model: how many games each model plays against the test players
-    """
-    model_names = os.listdir(ppo_saved_models_dir)
-    if len(model_names) > 1:
-        prev_network = PPONetwork(sess, PPOPlayer.input_dim, PPOPlayer.output_dim, "Prev_Network")
-        prev_player = PPOPlayer(hand_size, "Prev_Player", prev_network, sess)
-        prev_player.test_phase = True
-        curr_network = PPONetwork(sess, PPOPlayer.input_dim, PPOPlayer.output_dim, "Curr_Network")
-        parameters = joblib.load(os.path.join(ppo_saved_models_dir, model_names[0]))
-        curr_network.loadParams(parameters)
-        curr_player = PPOPlayer(hand_size, "Curr_Player", curr_network, sess)
-        curr_player.test_phase = True
-        training_player_1 = PPOPlayer(hand_size, "Training_Player_1", curr_network, sess)
-        training_player_2 = PPOPlayer(hand_size, "Training_Player_2", curr_network, sess)
-        training_player_3 = PPOPlayer(hand_size, "Training_Player_3", curr_network, sess)
-        test_players_lists = {"vs. Training Players": [training_player_1, training_player_2, training_player_3],
-                              "vs. 1 Defensive Player": [DefensivePlayer(hand_size, "Defensive Test Player")],
-                              "vs. 1 Aggressive Player": [AggressivePlayer(hand_size, "Aggressive Test Player")],
-                              "vs. 1 Random Player": [RandomPlayer(hand_size, "Random Test Player")],
-                              "vs. Previous Version": [prev_player]}
-        loss_ratios_per_test_list = {key: list() for key in test_players_lists.keys()}
-        for i in range(1, len(model_names)):
-            print("Testing model", i, "out of", len(model_names) - 1)
-            prev_network.loadParams(parameters)
-            parameters = joblib.load(os.path.join(ppo_saved_models_dir, model_names[i]))
-            curr_network.loadParams(parameters)
-            for key in test_players_lists:
-                print("Testing " + key + " ...")
-                loss_ratios = do_test_games([curr_player] + test_players_lists[key], test_games_per_model)
-                loss_ratios_per_test_list[key].append(loss_ratios[curr_player.name])
-        plt.figure(figsize=figure_size)
-        plt.title(title)
-        plt.xlabel("Player Model Number")
-        plt.ylabel("Loss Ratio in %")
-        i = 0
-        for key in loss_ratios_per_test_list:
-            plt.plot(loss_ratios_per_test_list[key], color=colors[i], label=key)
-            i += 1
-        plt.legend(loc=figure_legend_loc)
-        plt.savefig(title.replace('\n', '') + ".png")
+def test_and_plot_ppo():
+    games = 300
+    logging.basicConfig(filename='logs/PPOTester_log', level=logging.INFO)
+    indices, files = get_sorted_filenames_and_indices()
+    # run against 1 random player, save results, and plot graph
+    random_losses = []
+    for f in files:
+        random_losses.append(run_against_one_random(f, games))
+    plot_graph("PPO models loss ratio against 1 Random Agent", indices, random_losses)
+
+    # run against 3 random players, save results, and plot graph
+    random_3_losses = []
+    for f in files:
+        random_3_losses.append(run_against_3_randoms(f, games))
+    plot_graph("PPO models loss ratio against 3 Random Agents", indices, random_3_losses)
+
+    # run against 3 defensive players, save results, and plot graph
+    def_losses = []
+    for f in files:
+        def_losses.append(run_against_3_defensives(f, games))
+    plot_graph("PPO models loss ratio against 3 Defensive Agents", indices, def_losses)
+
+    # run against 3 aggressive players, save results, and plot graph
+    agg_losses = []
+    for f in files:
+        agg_losses.append(run_against_3_aggressives(f, games))
+    plot_graph("PPO models loss ratio against 3 Aggressive Agents", indices, agg_losses)
+
+    logging.shutdown()
 
 
 def generate_ppo_plots():
-    games_per_batch = 20
-    save_every = 5
-    num_models = 100  # number of models to save and test
-    test_games_per_saved_model = 50
-    with tf.compat.v1.Session() as sess:
-        train_ppo(sess, games_per_batch=games_per_batch, save_every=save_every, training_games=games_per_batch * save_every * num_models)
-        plot_trained_ppo_player_vs_test_cases(sess, "Loss Ratios of PPO Player", test_games_per_saved_model)
+    train_ppo()
+    test_and_plot_ppo()
 
 
 def plot(x_axis, y_axes, title: str, x_label: str, y_label: str, legend: bool):
