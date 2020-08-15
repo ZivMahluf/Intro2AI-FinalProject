61d3fbdf8cb79bc7764555203970d00962c20630
Vitaly Rajev <vitaly.rajev@gmail.com>
2020-08-15 20:43:11 +0300

    removed info field (env doesnt return it anymore)

--- PPOTrainer.py
+++ PPOTrainer.py
@@ -85,7 +85,6 @@ class PPOTrainer(object):
 
         # episode/training information
         self.totTrainingSteps = 0
-        self.epInfos = []
         self.gamesDone = 0
         self.losses = []
 
@@ -107,7 +106,7 @@ class PPOTrainer(object):
             available_actions = game.get_available_actions()
             action = turn_player.get_action(state, game.to_attack())
             value, neglogpac = turn_player.get_val_neglogpac()
-            new_state, reward, done, info = game.step(action)  # update the game
+            new_state, reward, done = game.step(action)  # update the game
 
             # add to list
             mb_obs.append(turn_player.last_converted_state.flatten())
@@ -118,7 +117,6 @@ class PPOTrainer(object):
             mb_rewards.append(reward)
             mb_dones.append(done)
             mb_availAcs.append(turn_player.last_converted_available_cards.flatten())
-            self.epInfos.append(info)
 
             # update current state
             state = new_state
@@ -238,6 +236,5 @@ class PPOTrainer(object):
                 name = "PPOParams/model" + str(update * self.games_per_batch)
                 self.trainingNetwork.saveParams(name)
                 joblib.dump(self.losses, "losses.pkl")
-                joblib.dump(self.epInfos, "epInfos.pkl")
 
             print("finished " + str(update * self.games_per_batch))
